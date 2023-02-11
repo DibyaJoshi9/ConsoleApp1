713ca5d94298e3cdccff59ff3ca9bb38534aea9f
DibyaJoshi <joshidibya77@gmail.com>
2023-02-11 20:29:33 +0545

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int Sum = p.Add(10, 20);
+            Console.WriteLine("Sum ={0}", Sum);
+
+        }
+        public int Add(int F, int S)
+        {
+            return F + S;
+
+
+
+        }
+    }
+
+}
